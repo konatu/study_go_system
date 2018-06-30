1d1dd6d4e8a1463dd526f72454e1dfbeb5ee80eb
konatu <syo.konatu@gmail.com>
2018-07-01 00:46:09 +0900

    create reader net_conn

--- io_reader/net_conn/main.go
+++ io_reader/net_conn/main.go
@@ -1,6 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
+	"net/http"
 	"os"
 )
+
+func main() {
+	conn, err := net.Dial("tcp", "test.jp:8008")
+	if err != nil {
+		panic(err)
+	}
+	conn.Write([]byte("GET /HTTP1.0\r\nHost:test.jp\r\n\r\n"))
+	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res.Header)
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+}
